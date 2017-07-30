b7aff86c4630f24e82fafa22117e6d4a69bd61c6
DivelNick <shabanovamir11@gmail.com>
2017-07-30 12:34:47 +0300

    change CircleImageView collapse animation

---
+++ app/src/main/java/ru/devtron/republicperi/utils/CircleImageView.java
@@ -0,0 +1,25 @@
+package ru.devtron.republicperi.utils;
+
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+public class CircleImageView extends de.hdodenhof.circleimageview.CircleImageView {
+
+    public CircleImageView(Context context) {
+        super(context);
+    }
+
+    public CircleImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CircleImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(heightMeasureSpec, heightMeasureSpec);
+    }
+}
